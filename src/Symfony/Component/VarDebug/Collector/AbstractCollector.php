e7abd6b0d2131028669a6c7987f23b742acb5afc
Nicolas Grekas <nicolas.grekas@gmail.com>
2014-04-05 20:35:00 +0200

    [VarDebug] algo to clone any PHP variable to a breadth-first queue

---
+++ src/Symfony/Component/VarDebug/Collector/AbstractCollector.php
@@ -0,0 +1,102 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\VarDebug\Collector;
+
+use Symfony\Component\VarDebug\Exception\ThrowingCasterException;
+
+/**
+ * @author Nicolas Grekas <p@tchwork.com>
+ */
+abstract class AbstractCollector implements CollectorInterface
+{
+    protected $maxItems = 500;
+    protected $maxString = 5000;
+
+    private $data = array(array(null));
+    private $prevErrorHandler = null;
+
+
+    public function setMaxItems($maxItems)
+    {
+        $this->maxItems = (int) $maxItems;
+    }
+
+    public function setMaxString($maxString)
+    {
+        $this->maxString = (int) $maxString;
+    }
+
+    public function collect($var)
+    {
+        $this->prevErrorHandler = set_error_handler(array($this, 'handleError'));
+        try {
+            $data = $this->doCollect($var);
+        } catch (\Exception $e) {
+            restore_error_handler();
+
+            throw $e;
+        }
+        restore_error_handler();
+        $this->prevErrorHandler = null;
+
+        return new Data($data);
+    }
+
+    abstract protected function doCollect($var);
+
+    protected function castObject($class, $obj)
+    {
+        $a = (array) $obj;
+
+        return $a;
+    }
+
+    protected function castResource($type, $res)
+    {
+        $a = array();
+
+        return $a;
+    }
+
+    private function callCaster($callback, $obj, $a)
+    {
+        try {
+            // Ignore invalid $callback
+            $callback = @call_user_func($callback, $obj, $a);
+
+            if (is_array($callback)) {
+                $a = $callback;
+            }
+        } catch (\Exception $e) {
+            $a["\0~\0⚠"] = new ThrowingCasterException($callback, $e);
+        }
+
+        return $a;
+    }
+
+    /**
+     * @internal
+     */
+    public function handleError($type, $msg, $file, $line, $context)
+    {
+        if (E_RECOVERABLE_ERROR === $type || E_USER_ERROR === $type) {
+            // Collector never dies
+            throw new \ErrorException($msg, 0, $type, $file, $line);
+        }
+
+        if ($this->prevErrorHandler) {
+            return call_user_func_array($this->prevErrorHandler, array($type, $msg, $file, $line, $context));
+        }
+
+        return false;
+    }
+}
---
+++ src/Symfony/Component/VarDebug/Exception/ThrowingCasterException.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+* This file is part of the Symfony package.
+*
+* (c) Fabien Potencier <fabien@symfony.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Symfony\Component\VarDebug\Exception;
+
+/**
+ * @author Nicolas Grekas <p@tchwork.com>
+ */
+class ThrowingCasterException extends \Exception
+{
+    private $caster;
+
+    public function __construct($caster, \Exception $prev)
+    {
+        $this->caster = $caster;
+        parent::__construct(null, 0, $prev);
+    }
+}
